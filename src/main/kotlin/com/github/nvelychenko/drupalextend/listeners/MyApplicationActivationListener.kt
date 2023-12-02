b6a1d702ae4183138f8661592a5993fb739c47d0
GitHub Action <action@github.com>
2023-12-02 11:13:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/nvelychenko/drupalextend/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nvelychenko/drupalextend/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nvelychenko.drupalextend.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
