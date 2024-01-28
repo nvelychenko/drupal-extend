62b23616e338a161a635d53a2b211b34ff17f6f2
Nazar Velychenko <n.velychenko@dev-branch.com>
2024-01-28 20:10:17 +0200

    fix: Added ability to specify whole id, instead of relying on predefined patter.

--- src/main/kotlin/com/github/nvelychenko/drupalextend/util/PhpDocUtils.kt
+++ src/main/kotlin/com/github/nvelychenko/drupalextend/util/PhpDocUtils.kt
@@ -1,8 +1,7 @@
 package com.github.nvelychenko.drupalextend.util
 
-@Suppress("RegExpUnnecessaryNonCapturingGroup")
 fun getPhpDocParameter(phpDocText: String, id: String): String? {
-    val entityTypeMatch = Regex("${id}(?:\"?)\\s*=\\s*\"([^\"]+)\"").find(phpDocText)
+    val entityTypeMatch = Regex("${id}?\\s*=\\s*\"([^\"]+)\"").find(phpDocText)
 
     return entityTypeMatch?.groups?.get(1)?.value
 }
