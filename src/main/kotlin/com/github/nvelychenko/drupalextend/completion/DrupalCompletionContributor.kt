09051505499959d449c9024be2341c8189223522
Nazar Velychenko <n.velychenko@dev-branch.com>
2024-01-28 20:09:09 +0200

    feat: Refactored DrupalCompletionContributor, fixed few bugs.

--- src/main/kotlin/com/github/nvelychenko/drupalextend/completion/DrupalCompletionContributor.kt
+++ src/main/kotlin/com/github/nvelychenko/drupalextend/completion/DrupalCompletionContributor.kt
@@ -1,32 +1,52 @@
 package com.github.nvelychenko.drupalextend.completion
 
-import com.github.nvelychenko.drupalextend.extensions.isSuperInterfaceOf
-import com.github.nvelychenko.drupalextend.index.ConfigEntityIndex
+import com.github.nvelychenko.drupalextend.completion.providers.EntityStorageProvider
+import com.github.nvelychenko.drupalextend.completion.providers.FieldCompletionProvider
+import com.github.nvelychenko.drupalextend.completion.providers.MagicPropertyFieldCompletionProvider
+import com.github.nvelychenko.drupalextend.extensions.getAllProjectKeys
+import com.github.nvelychenko.drupalextend.extensions.getValue
 import com.github.nvelychenko.drupalextend.index.ContentEntityFqnIndex
-import com.github.nvelychenko.drupalextend.index.ContentEntityIndex
 import com.github.nvelychenko.drupalextend.index.FieldTypeIndex
 import com.github.nvelychenko.drupalextend.index.FieldsIndex
+import com.github.nvelychenko.drupalextend.patterns.Patterns
 import com.github.nvelychenko.drupalextend.type.EntityStorageTypeProvider
-import com.github.nvelychenko.drupalextend.extensions.getAllProjectKeys
-import com.github.nvelychenko.drupalextend.extensions.getAllValuesWithKeyPrefix
-import com.github.nvelychenko.drupalextend.extensions.getValue
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.util.ProcessingContext
 import com.intellij.util.indexing.FileBasedIndex
 import com.jetbrains.php.PhpClassHierarchyUtils
 import com.jetbrains.php.PhpIndex
 import com.jetbrains.php.lang.PhpLanguage
 import com.jetbrains.php.lang.lexer.PhpTokenTypes
-import com.jetbrains.php.lang.parser.PhpElementTypes
-import com.jetbrains.php.lang.psi.elements.*
+import com.jetbrains.php.lang.psi.elements.ArrayAccessExpression
+import com.jetbrains.php.lang.psi.elements.FieldReference
+import com.jetbrains.php.lang.psi.elements.MethodReference
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression
 
 class DrupalCompletionContributor : CompletionContributor() {
 
     init {
+        // \Drupal::entityTypeManager->getStorage('no|
+        extend(CompletionType.BASIC, Patterns.LEAF_INSIDE_METHOD_PARAMETER, EntityStorageProvider())
+
+        // $node->field_|
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement().withLanguage(PhpLanguage.INSTANCE),
+            MagicPropertyFieldCompletionProvider()
+        )
+
+        // $node->set('fi
+        // $node->get('fi|
+        extend(CompletionType.BASIC, Patterns.LEAF_INSIDE_METHOD_PARAMETER, FieldCompletionProvider())
+
+        // $node->get('field_entity')->entity
+        // $node->get('field_entity')[0]->entity
+        // $node->get('field_entity')->first()->entity
+        // @todo Fix completion for base fields that are not inside main entity.
+        // e.g. 'status' -> \Drupal\Core\Entity\EntityPublishedTrait::publishedBaseFieldDefinitions
         extend(
             CompletionType.BASIC,
             PlatformPatterns.psiElement().withLanguage(PhpLanguage.INSTANCE),
@@ -40,259 +60,79 @@ class DrupalCompletionContributor : CompletionContributor() {
                     val parent: PsiElement
                     val prevSibling = completionParameters.position.prevSibling
                     parent = if (prevSibling != null && prevSibling.node.elementType === PhpTokenTypes.ARROW) {
-                        // $xex->
-                        prevSibling.parent
+                        when (prevSibling.parent) {
+                            is FieldReference -> prevSibling.parent
+                            else -> return
+                        }
                     } else {
-                        // $xex->ad
                         psiElement.parent
                     }
 
                     if (parent !is FieldReference) return
-                    val classReference = parent.classReference
-                    if (classReference !is PhpReference) return
-
-                    getFieldCompletionForClassReference(classReference, completionResultSet)
-                }
-            })
-
-        extend(
-                CompletionType.BASIC,
-                PlatformPatterns.psiElement().withLanguage(PhpLanguage.INSTANCE),
-                object : CompletionProvider<CompletionParameters>() {
-                    public override fun addCompletions(
-                            completionParameters: CompletionParameters,
-                            processingContext: ProcessingContext,
-                            completionResultSet: CompletionResultSet
-                    ) {
-                        val psiElement = completionParameters.originalPosition ?: return
-                        val parent: PsiElement
-                        val prevSibling = completionParameters.position.prevSibling
-                        parent = if (prevSibling != null && prevSibling.node.elementType === PhpTokenTypes.ARROW) {
-                            when (prevSibling.parent) {
-                                is FieldReference -> prevSibling.parent
-                                else -> return
-                            }
-                        } else {
-                            psiElement.parent
-                        }
-
-                        if (parent !is FieldReference) return
-                        val methodReference = when (parent.classReference) {
-                            is MethodReference -> when ((parent.classReference as MethodReference).name) {
-                                    "get" -> parent.classReference
-                                    "first" -> (parent.classReference as MethodReference).classReference
-                                    else -> return
-                                }
-                            is ArrayAccessExpression -> parent.classReference?.children?.firstOrNull()
+                    val methodReference = when (parent.classReference) {
+                        is MethodReference -> when ((parent.classReference as MethodReference).name) {
+                            "get" -> parent.classReference
+                            "first" -> (parent.classReference as MethodReference).classReference
                             else -> return
                         }
-                        if (methodReference !is MethodReference) return
 
-                        val entityType: String?
-                        val index = FileBasedIndex.getInstance()
-                        val project = psiElement.project
-                        if (methodReference.signature.contains(EntityStorageTypeProvider.Util.SPLIT_KEY)) {
-                            entityType = methodReference.signature.substringAfter(EntityStorageTypeProvider.Util.SPLIT_KEY).substringBefore('.')
-                        } else {
-                            val globalTypes = methodReference.classReference?.globalType?.types?.map { it.replace("[]", "") }
-
-                            if (globalTypes.isNullOrEmpty()) return
-
-                            val entityTypeFqn = index.getAllProjectKeys(
-                                    ContentEntityFqnIndex.KEY, project
-                            ).find { globalTypes.contains(it) } ?: return
-
-                            val contentEntity = index.getValue(ContentEntityFqnIndex.KEY, entityTypeFqn, project) ?: return
-                            entityType = contentEntity.entityTypeId
-                        }
-
-                        val fieldName = (methodReference.parameters[0] as StringLiteralExpression).contents
-                        if (entityType.isEmpty() || fieldName.isEmpty()) return
-
-                        val fieldInstance = FileBasedIndex.getInstance().getValue(FieldsIndex.KEY, "${entityType}|${fieldName}", project) ?: return
-                        val fieldType = FileBasedIndex.getInstance().getValue(FieldTypeIndex.KEY, fieldInstance.fieldType, project) ?: return
-
-                        val properties = HashMap<String, String>()
-                        properties.putAll(fieldType.properties)
-
-                        val fieldClassInstance = PhpIndex.getInstance(project).getClassesByFQN(fieldType.fqn).firstOrNull() ?: return
-                        PhpClassHierarchyUtils.processSuperClasses(fieldClassInstance, false, true) {
-                            it.findOwnMethodByName("propertyDefinitions") ?: return@processSuperClasses true
-                            val superProperties = index.getValue(FieldTypeIndex.KEY, it.fqn, project)
-                            superProperties?.let { _ -> properties.putAll(superProperties.properties) }
-                            return@processSuperClasses true
-                        }
-
-                        properties.forEach {
-                            completionResultSet.addElement(
-                                PrioritizedLookupElement.withPriority(
-                                        LookupElementBuilder.create(it.key).withTypeText(it.value).withBoldness(true),
-                                        Double.MAX_VALUE,
-                                ),
-                            )
-                        }
-
+                        is ArrayAccessExpression -> parent.classReference?.children?.firstOrNull()
+                        else -> return
                     }
-                })
+                    if (methodReference !is MethodReference) return
 
-        extend(
-            CompletionType.BASIC,
-            PlatformPatterns
-                .psiElement(LeafPsiElement::class.java)
-                .withParent(
-                    PlatformPatterns.psiElement(StringLiteralExpression::class.java)
-                        .withParent(
-                            PlatformPatterns.psiElement(PhpElementTypes.PARAMETER_LIST)
-                                .withParent(PlatformPatterns.psiElement(PhpElementTypes.METHOD_REFERENCE))
-                        )
-                )
-                .withLanguage(PhpLanguage.INSTANCE),
-            object : CompletionProvider<CompletionParameters>() {
-                public override fun addCompletions(
-                    completionParameters: CompletionParameters,
-                    processingContext: ProcessingContext,
-                    completionResultSet: CompletionResultSet
-                ) {
-                    val leaf = completionParameters.originalPosition ?: return
-                    val element = leaf.parent
+                    val entityType: String?
+                    val index = FileBasedIndex.getInstance()
+                    val project = psiElement.project
+                    if (methodReference.signature.contains(EntityStorageTypeProvider.Util.SPLIT_KEY)) {
+                        entityType = methodReference.signature.substringAfter(EntityStorageTypeProvider.Util.SPLIT_KEY)
+                            .substringBefore('.')
+                    } else {
+                        val globalTypes =
+                            methodReference.classReference?.globalType?.types?.map { it.replace("[]", "") }
 
-                    if (element !is StringLiteralExpression) return
+                        if (globalTypes.isNullOrEmpty()) return
 
-                    val parameterList = (element.parent as ParameterList)
-                    val methodReference = (parameterList.parent as MethodReference)
+                        val entityTypeFqn = index.getAllProjectKeys(
+                            ContentEntityFqnIndex.KEY, project
+                        ).find { globalTypes.contains(it) } ?: return
 
-                    if ("get" != methodReference.name) return
-
-                    var classReference = methodReference.classReference
-
-                    if (classReference !is PhpReference) {
-                        if (classReference !is ArrayAccessExpression || classReference.value !is PhpReference) {
-                            return
-                        }
-
-                        classReference = classReference.value as PhpReference
+                        val contentEntity = index.getValue(ContentEntityFqnIndex.KEY, entityTypeFqn, project) ?: return
+                        entityType = contentEntity.entityTypeId
                     }
 
-                    getFieldCompletionForClassReference(classReference, completionResultSet)
-                }
-            })
+                    val fieldName = (methodReference.parameters[0] as StringLiteralExpression).contents
+                    if (entityType.isEmpty() || fieldName.isEmpty()) return
 
-        extend(
-            CompletionType.BASIC,
-            PlatformPatterns
-                .psiElement(LeafPsiElement::class.java)
-                .withParent(
-                    PlatformPatterns.psiElement(StringLiteralExpression::class.java)
-                        .withParent(
-                            PlatformPatterns.psiElement(PhpElementTypes.PARAMETER_LIST)
-                                .withParent(PlatformPatterns.psiElement(PhpElementTypes.METHOD_REFERENCE))
-                        )
-                )
-                .withLanguage(PhpLanguage.INSTANCE),
-            object : CompletionProvider<CompletionParameters>() {
-                public override fun addCompletions(
-                    completionParameters: CompletionParameters,
-                    processingContext: ProcessingContext,
-                    completionResultSet: CompletionResultSet
-                ) {
-                    val leaf = completionParameters.originalPosition ?: return
-                    val element = leaf.parent
-                    if (element !is StringLiteralExpression || element.contents.isEmpty()) return
-
-                    val parameterList = element.parent as ParameterList
-                    if (!parameterList.parameters.first().isEquivalentTo(element)) {
-                        return
-                    }
-                    val methodReference = parameterList.parent as MethodReference
-
-                    val allowedMethods = arrayOf(
-                        "getAccessControlHandler",
-                        "getStorage",
-                        "getViewBuilder",
-                        "getListBuilder",
-                        "getFormObject",
-                        "getRouteProviders",
-                        "hasHandler",
-                        "getDefinition",
-                    )
-
-                    if (!allowedMethods.contains(methodReference.name)) return
-                    val method = methodReference.resolve() ?: return
-
-                    if (method !is Method) return
-
-                    val methodClass = method.containingClass as PhpClass
-                    val project = method.project
-
-                    val entityReferences =
-                        PhpIndex.getInstance(project)
-                            .getInterfacesByFQN("\\Drupal\\Core\\Entity\\EntityTypeManagerInterface").firstOrNull()
+                    val fieldInstance =
+                        FileBasedIndex.getInstance().getValue(FieldsIndex.KEY, "${entityType}|${fieldName}", project)
+                            ?: return
+                    val fieldType =
+                        FileBasedIndex.getInstance().getValue(FieldTypeIndex.KEY, fieldInstance.fieldType, project)
                             ?: return
 
-                    if (!methodClass.isSuperInterfaceOf(entityReferences)) return
+                    val properties = HashMap<String, String>()
+                    properties.putAll(fieldType.properties)
 
-                    val instance = FileBasedIndex.getInstance()
+                    val fieldClassInstance =
+                        PhpIndex.getInstance(project).getClassesByFQN(fieldType.fqn).firstOrNull() ?: return
+                    PhpClassHierarchyUtils.processSuperClasses(fieldClassInstance, false, true) {
+                        it.findOwnMethodByName("propertyDefinitions") ?: return@processSuperClasses true
+                        val superProperties = index.getValue(FieldTypeIndex.KEY, it.fqn, project)
+                        superProperties?.let { _ -> properties.putAll(superProperties.properties) }
+                        return@processSuperClasses true
+                    }
 
-                    instance
-                        .getAllProjectKeys(ContentEntityIndex.KEY, project)
-                        .filter { !it.contains("\\") }
-                        .forEach {
-                            val contentEntity = instance.getValue(ContentEntityIndex.KEY, it, project) ?: return@forEach
-
-                            completionResultSet.addElement(
-                                LookupElementBuilder.create(it)
-                                    .withTypeText(contentEntity.fqn, true)
-                            )
-                        }
-
-                    instance
-                        .getAllProjectKeys(ConfigEntityIndex.KEY, project)
-                        .forEach {
-                            val configEntityFqn = instance.getValue(ConfigEntityIndex.KEY, it, project) ?: return@forEach
-
-                            completionResultSet.addElement(
-                                LookupElementBuilder.create(it)
-                                    .withTypeText(configEntityFqn, true)
-                            )
-                        }
+                    properties.forEach {
+                        completionResultSet.addElement(
+                            PrioritizedLookupElement.withPriority(
+                                LookupElementBuilder.create(it.key).withTypeText(it.value).withBoldness(true),
+                                Double.MAX_VALUE,
+                            ),
+                        )
+                    }
                 }
             })
     }
 
-    fun getFieldCompletionForClassReference(classReference: PhpReference, completionResultSet: CompletionResultSet) {
-        val project = classReference.project
-        val index = FileBasedIndex.getInstance()
-
-        val entityTypeId: String?
-
-        val globalTypes = classReference.globalType.types.map { it.replace("[]", "") }
-
-        if (globalTypes.isEmpty()) return
-
-        val entityTypeFqn = index
-            .getAllProjectKeys(ContentEntityFqnIndex.KEY, project)
-            .find { globalTypes.contains(it) } ?: return
-
-        val contentEntity = index.getValue(ContentEntityFqnIndex.KEY, entityTypeFqn, project) ?: return
-        entityTypeId = contentEntity.entityTypeId
-
-        val keys = index.getValue(ContentEntityIndex.KEY, entityTypeId, project)?.keys ?: return
-
-        index.getAllValuesWithKeyPrefix(FieldsIndex.KEY, "$entityTypeId|", project)
-            .forEach {
-                var fieldName = it.fieldName
-                if (fieldName.contains("KEY|")) {
-                    val key = fieldName.split("|")[1]
-                    if (keys.contains(key)) {
-                        return@forEach
-                    }
-                    fieldName = keys[key] ?: return@forEach
-                }
-
-                completionResultSet.addElement(
-                    LookupElementBuilder.create(fieldName)
-                        .withTypeText(it.fieldType, true)
-                )
-            }
-    }
 }
---
+++ src/main/kotlin/com/github/nvelychenko/drupalextend/completion/providers/FieldCompletionProvider.kt
@@ -0,0 +1,130 @@
+package com.github.nvelychenko.drupalextend.completion.providers
+
+import com.github.nvelychenko.drupalextend.data.ExtendableContentEntityRelatedClasses
+import com.github.nvelychenko.drupalextend.extensions.getAllProjectKeys
+import com.github.nvelychenko.drupalextend.extensions.getAllValuesWithKeyPrefix
+import com.github.nvelychenko.drupalextend.extensions.getValue
+import com.github.nvelychenko.drupalextend.index.ContentEntityFqnIndex
+import com.github.nvelychenko.drupalextend.index.ContentEntityIndex
+import com.github.nvelychenko.drupalextend.index.FieldsIndex
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.PrioritizedLookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.util.ProcessingContext
+import com.intellij.util.indexing.FileBasedIndex
+import com.jetbrains.php.PhpClassHierarchyUtils
+import com.jetbrains.php.PhpIndex
+import com.jetbrains.php.lang.psi.elements.*
+
+/**
+ * Provides autocompletion for fields.
+ *
+ * $node->get('field_|
+ * $node->set('field_|
+ */
+open class FieldCompletionProvider : CompletionProvider<CompletionParameters>() {
+
+    open val priority = 10.0
+
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        val element = (parameters.originalPosition ?: return).parent
+
+        if (element !is StringLiteralExpression) return
+
+        val parameterList = element.parent as ParameterList
+
+        if (!parameterList.parameters.first().isEquivalentTo(element)) return
+
+        val methodReference = parameterList.parent as MethodReference
+
+        if (!arrayOf("get", "set").contains(methodReference.name)) return
+
+        processMemberReference(methodReference, result)
+    }
+
+    fun processMemberReference(memberReference: MemberReference, result: CompletionResultSet) {
+        when (val classReference = memberReference.classReference) {
+            is PhpReference -> getFieldCompletionForClassReference(classReference, result)
+            is ArrayAccessExpression -> (classReference.value as? PhpReference)?.let {
+                getFieldCompletionForClassReference(it, result, true)
+            }
+        }
+    }
+
+    private fun getFieldCompletionForClassReference(
+        classReference: PhpReference,
+        result: CompletionResultSet,
+        isArrayAccessExpression: Boolean = false
+    ) {
+        val project = classReference.project
+        val index = FileBasedIndex.getInstance()
+
+        val globalTypes = classReference.globalType.types.map {
+            // Situation is following, there is array of nodes. Node[]
+            // Only in case if classReference is taken from ArrayAccessExpression
+            // e.g. $nodes[0]->field we want to remove "[]" from type. Otherwise,
+            // autocomplete will work for $nodes->field too.
+            it.takeIf { isArrayAccessExpression }?.replace("[]", "") ?: it
+        }.takeIf { it.isNotEmpty() } ?: return
+
+        val contentEntity = index.getAllProjectKeys(ContentEntityFqnIndex.KEY, project)
+            .find { globalTypes.contains(it) }
+            ?.let { index.getValue(ContentEntityFqnIndex.KEY, it, project) } ?: return
+
+        val entityTypeId = contentEntity.entityTypeId
+        val keys = index.getValue(ContentEntityIndex.KEY, entityTypeId, project)?.keys?.toMutableMap() ?: return
+
+        // Basic field definition usually are preset in subclasses here we
+        // process all supers (traits, classes) to find if there is additional
+        // field to autocomplete.
+        // @see com.github.nvelychenko.drupalextend.data.ExtendableContentEntityRelatedClasses.allPossibleExtendableContentEntityClasses
+        val additionalClasses = mutableListOf<String>()
+        PhpIndex.getInstance(project).getClassesByFQN(contentEntity.fqn)
+            .firstOrNull()
+            ?.let { contentEntityClass ->
+                PhpClassHierarchyUtils.processSupers(contentEntityClass, false, true) { supper ->
+                    if (ExtendableContentEntityRelatedClasses.hasClass(supper.fqn)) {
+                        additionalClasses.add(supper.fqn)
+                    }
+                    true
+                }
+            }
+
+        index.getAllValuesWithKeyPrefix(FieldsIndex.KEY, "$entityTypeId|", project)
+            .toMutableList()
+            // Merge additional fields.
+            .let { main ->
+                additionalClasses.forEach {
+                    main.addAll(index.getAllValuesWithKeyPrefix(FieldsIndex.KEY, "$it|", project))
+                }
+
+                main
+            }
+            .forEach { drupalField ->
+                val name = drupalField.fieldName
+                when {
+                    // For cases when field key is
+                    // $entity_type->getKey('owner') => BaseFieldDefinition::create('entity_reference')
+                    // we use special prefix KEY| that later is replaced by actual key from annotations.
+                    name.contains("KEY|") -> keys[name.substringAfter("|")]
+                    else -> name
+                }?.let { fieldName ->
+                    result.addElement(
+                        PrioritizedLookupElement.withPriority(
+                            LookupElementBuilder.create(fieldName).withTypeText(drupalField.fieldType, true),
+                            priority
+                        )
+
+                    )
+                }
+
+            }
+    }
+
+}
